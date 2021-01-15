389a3cb652b6a7a454da7b29b3197f48bbc06a49
la-salman <salmanhaikal10@gmail.com>
2021-01-15 07:30:34 +0000

    login section

--- src/components/Router/pages/Login.js
+++ src/components/Router/pages/Login.js
@@ -11,7 +11,7 @@ let Login = ()=> {
                       createElement("div", {class: "container"}, createElement("img", {src:brand, class: "brand"}),createElement("div",{class: "wrapper-form"},
                       createElement("form", {class: "form"}, createElement("div", {class: "form-container"}, 
                       createElement("h1", {class: "heading-form"}, "Sign In"),createElement("input", {placeholder: "Email Or Phone number", class:"field-input",type: "text"}),
-                      createElement("input", {placeholder: "pasword", class:"field-input",type: "Pasword"})),
+                      createElement("input", {placeholder: "pasword", class:"field-input",type: "Password"})),
                       createElement("button",{class: "btn-form",onclick:()=>{alert("user is sign in")}},createElement("a",{href:"/home",style:"color:white;text-decoration:none"},"Sign In")),createElement("input", {type: "checkbox", class: "checkbox"}),
                       createElement("label",null, "Remember Me"),createElement("label",{class: "label-need"}, "Need help?")),
                       createElement("span",null,createElement("img", {src:fbPic, class: "img-fb"}), createElement("a",{class: "label-img"},"Login With Facebook")),
